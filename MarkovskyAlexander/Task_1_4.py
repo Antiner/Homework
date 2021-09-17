2e531780e65aa555fe4f588d4fea9f2d963dcb54
Antiner <59028876+Antiner@users.noreply.github.com>
2021-09-17 14:17:03 +0300

    Delete MarkovskyAlexander directory

--- MarkovskyAlexander/Task_1_4.py
+++
@@ -1,6 +0,0 @@
-# Task 1.4
-# Write a Python program to sort a dictionary by key.
-
-def task_1_4(dictionary):
-    for key in sorted(dictionary):
-        print("%s: %s" % (key, dictionary[key]))
--- MarkovskyAlexander/Task_1_5.py
+++
@@ -1,9 +0,0 @@
-# Task 1.5
-# Write a Python program to print all unique values of all dictionaries in a list.
-
-def task_1_5(array):
-    list_1 = []
-    for i in array:
-        for v in i:
-            list_1.append(i.get(v))
-    print(set(list_1))
