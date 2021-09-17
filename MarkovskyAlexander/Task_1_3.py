2befa021e09a97ddb4e0511a74f1c14414460ca0
Antiner <59028876+Antiner@users.noreply.github.com>
2021-09-17 13:38:38 +0300

    Delete MarkovskyAlexander directory

--- MarkovskyAlexander/Task_1_3.py
+++
@@ -1,6 +0,0 @@
-# Task 1.3
-# Write a Python program that accepts a comma separated sequence of words as input and prints the unique /
-# words in sorted form.
-
-def task_1_3(*args):
-    print(sorted(set(*args)))
